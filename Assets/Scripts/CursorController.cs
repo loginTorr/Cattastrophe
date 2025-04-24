d2329c5480c6608190d8b1b58d8e02f12825c99b
gabethomp <gabealan9@gmail.com>
2025-04-23 19:49:02 -0500

    made cursor change when clicked

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorController : MonoBehaviour {
-    [SerializeField] private Texture2D CursorTextureDefault;
-    [SerializeField] private Vector2 ClickPosition = Vector2.zero;
+    [Header("Cursors")]
+    public Texture2D CursorDefault, CursorClick;
+    [Header("Other")]
+    public Vector2 ClickPosition = Vector2.zero;
+    public float Duration = 0.5f;
+    public bool IsClicking = false;
 
     void Start() {
-        Cursor.SetCursor(CursorTextureDefault, ClickPosition, CursorMode.Auto);
+        Cursor.SetCursor(CursorDefault, ClickPosition, CursorMode.Auto);
     }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0) && !IsClicking) {
+            StartCoroutine(ChangeCursor());
+        }
+    }
+
+    IEnumerator ChangeCursor() { 
+            IsClicking = true;
+            Cursor.SetCursor(CursorClick, ClickPosition, CursorMode.Auto);
+            yield return new WaitForSeconds(Duration);
+            IsClicking = false;
+            Cursor.SetCursor(CursorDefault, ClickPosition, CursorMode.Auto);
+        }
 }
