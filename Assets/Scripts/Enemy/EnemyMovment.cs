b56833aeadf3ffee0cbd8c35baf268132a785e3d
ChicknNWaffles <elora.browning@gmail.com>
2025-04-24 22:54:30 -0500

    Set up state machine to react to enemy's proximity to the player
    I also put the enemy movement script in the enemy folder and the range script in the player folder

---
+++ Assets/Scripts/Enemy/EnemyMovment.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovment : MonoBehaviour
+{
+    public enum State{
+        TooClose,
+        Mele,
+        CloseShooting,
+        MidShooting,
+        FarShooting,
+        Agro,
+        Wandering
+    }
+    public State state;
+    public GameObject Player;
+    private Vector3 PlayerPos;
+    private Range range;
+    public float AgroSpeed;
+    public float WanderingSpeed;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        PlayerPos = Player.transform.position;
+        range = Player.GetComponent<Range>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        PlayerPos = Player.transform.position;
+        ChangeState();
+    }
+
+    void ChangeState() {
+        float distance = Vector3.Distance(transform.position, PlayerPos);
+        if (distance <= range.TooClose) {
+            state = State.TooClose;
+        }else if (distance <= range.Mele) {
+            state = State.Mele;
+        }else if (distance <= range.CloseShooting) {
+            state = State.CloseShooting;
+        }else if (distance <= range.MidShooting) {
+            state = State.MidShooting;
+        }else if (distance <= range.FarShooting) {
+            state = State.FarShooting;
+        }else if (distance <= range.Agro) {
+            state = State.Agro;
+        }else {
+            state = State.Wandering;
+        }
+    }
+}
