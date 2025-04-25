1700309e1f5ad29a85da3b57c2a71ee4fa0fbd2e
gabethomp <gabealan9@gmail.com>
2025-04-25 15:15:37 -0500

    more implemented room spawning and tp

--- Assets/Scripts/Enironment/RoomSpawner.cs
+++ Assets/Scripts/Enironment/RoomSpawner.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour {
     [Header("Rooms To Spawn")]
     public List<GameObject> RoomList = new List<GameObject>();
+    [Header("Other")]
+    public float HeightOffset = 100f;
+    // for tping in this script
+    public Transform Player;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.H)) { SpawnRoom(); }
@@ -23,12 +27,14 @@ public class RoomSpawner : MonoBehaviour {
     IEnumerator ChooseAndSpawn() {
         // maybe could lock player controls this coroutine starts so it wont mess with the tp or room spawning
         int RandIndex = Random.Range(0, RoomList.Count);
+        Vector3 SpawnPosition = transform.position + Vector3.up * HeightOffset;
         GameObject NextRoom = RoomList[RandIndex];
-        GameObject Room = Instantiate(NextRoom, transform.position, Quaternion.identity);
+        GameObject Room = Instantiate(NextRoom, SpawnPosition, Quaternion.identity);
         RoomList.RemoveAt(RandIndex);
-        // for testing
-        yield return new WaitForSeconds(3);
-        Destroy(Room);
+        // tps player but we can add this to rooms themselves later
+        yield return new WaitForSeconds(1);
+        Player.position += Vector3.up * HeightOffset;
+        
 
     }
 }
