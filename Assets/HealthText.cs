50a976d029677d62c1962d1c736ebc6cab1e15ec
Quentin Quarles <quentavius115@gmail.com>
2025-05-08 18:48:53 -0500

    pause menu with stats in my scene

---
+++ Assets/HealthText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class HealthText : MonoBehaviour
+{
+    public PlayerMovement healthCheck;
+    public TextMeshProUGUI health;
+    public TextMeshProUGUI speed;
+    public TextMeshProUGUI attack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.text = "Health: " + healthCheck.CurHealth + "/" + healthCheck.MaxHealth;
+        speed.text = "Speed: " + healthCheck.MaxSpeed;
+        attack.text = "Attack: " + healthCheck.AttackDamage;
+
+    }
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool enter = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(enter){
+                pauseMenu.SetActive(true);
+                enter = false;
+                Time.timeScale = 0;
+            }
+            else{
+                pauseMenu.SetActive(false);
+                enter = true;
+                Time.timeScale = 1;
+            }
+            
+            
+        }
+    }
+}
