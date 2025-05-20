dc4f9b1b6dc5ccee394220704611a0e30675f3d5
gabethomp <gabealan9@gmail.com>
2025-05-20 11:57:25 -0500

    fixed cursor

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -7,7 +7,7 @@ public class CursorController : MonoBehaviour {
     public Texture2D CursorDefault, CursorClick;
     [Header("Other")]
     public Vector2 ClickPosition = Vector2.zero;
-    public float Duration = 4f;
+    public float Duration = 0.5f;
     public bool IsClicking = false;
 
     void Start() {
@@ -15,7 +15,7 @@ public class CursorController : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetMouseButtonDown(1) && !IsClicking) {
+        if (Input.GetMouseButtonDown(0) && !IsClicking) {
             StartCoroutine(ChangeCursor());
         }
     }
