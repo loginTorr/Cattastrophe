491dd752bb6e8000cdd0f1405d53140db1c6ecda
Logan Torr <113713716+loginTorr@users.noreply.github.com>
2025-05-20 14:56:43 -0500

    Medium Room fix

--- Assets/Scripts/ApplyShader.cs
+++ Assets/Scripts/ApplyShader.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static Unity.VisualScripting.Metadata;
 
 public class ApplyShader : MonoBehaviour{
     // for most shaders this list will only have one object,
