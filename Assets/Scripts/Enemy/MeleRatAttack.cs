47f529b32fa0534a57bef0fb02a4e9ab2b593f7a
ChicknNWaffles <elora.browning@gmail.com>
2025-05-20 06:22:40 -0500

    Updated Mele Rat Prefab
    They attack more often and they're swords are longer. Hopefully this will address some of the comments from Beta Testing.

--- Assets/Scripts/Enemy/MeleRatAttack.cs
+++ Assets/Scripts/Enemy/MeleRatAttack.cs
@@ -15,7 +15,7 @@ public class MeleRatAttack : MonoBehaviour{
     // Start is called before the first frame update
     void Start(){
         Self = transform.gameObject.GetComponent<EnemyStateInfo>();
-        AttackTimer = Random.Range(MinAttackPause, MaxAttackPause);
+        AttackTimer = 0;
         print("Remember! switch out the name for the weapon object at the end of the path once the actual model is in place. It currently says 'Cube', which won't work if the object you're trying to grab isn't called 'Cube'");
         WeaponCollider = transform.Find("mixamorig:Hips").Find("mixamorig:Spine").Find("mixamorig:Spine1").Find("mixamorig:Spine2").Find("mixamorig:RightShoulder").Find("mixamorig:RightArm").Find("mixamorig:RightForeArm").Find("mixamorig:RightHand").Find("Cube").GetComponent<BoxCollider>();
         Anim = transform.gameObject.GetComponent<Animator>();
